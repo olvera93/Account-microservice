7c70220751014e71c53f3f39361de90487ddd54c
olvera93 <olvera9302@outlook.com>
2025-05-03 20:09:52 -0600

    [ADD] Schemas for DTOS

--- src/main/java/com/olvera/accounts/controller/AccountController.java
+++ src/main/java/com/olvera/accounts/controller/AccountController.java
@@ -1,9 +1,14 @@
 package com.olvera.accounts.controller;
 
 import com.olvera.accounts.dto.CustomerDto;
+import com.olvera.accounts.dto.ErrorResponseDto;
 import com.olvera.accounts.dto.ResponseDto;
 import com.olvera.accounts.service.IAccountsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -32,6 +37,24 @@ public class AccountController {
             summary = "Create Account REST API",
             description = "REST API to create a new Customer & Account inside EazyBank"
     )
+    @ApiResponses(value = {
+
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 
@@ -42,6 +65,23 @@ public class AccountController {
                 .body(new ResponseDto(STATUS_201, MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Account Details REST API",
+            description = "REST API to fetch Customer & Account details based on a mobile number"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(
             @RequestParam
@@ -55,6 +95,32 @@ public class AccountController {
                 .body(customerDto);
     }
 
+    @Operation(
+            summary = "Update Account Details REST API",
+            description = "REST API to update Customer & Account details based on a mobile number"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP Status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Expectation Failed",
+                            content = @Content(
+                                    schema = @Schema(implementation = ErrorResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status Internal Server Error",
+                            content = @Content(
+                                    schema = @Schema(implementation = ErrorResponseDto.class)
+                            )
+                    )
+            }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -65,11 +131,38 @@ public class AccountController {
                     .body(new ResponseDto(STATUS_200, MESSAGE_200));
         } else {
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(STATUS_500, MESSAGE_500));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(STATUS_417, MESSAGE_417_UPDATE));
         }
     }
 
+
+    @Operation(
+            summary = "Delete Account Details REST API",
+            description = "REST API to delete Customer & Account details based on a mobile number"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP Status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Expectation Failed",
+                            content = @Content(
+                                    schema = @Schema(implementation = ErrorResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status Internal Server Error",
+                            content = @Content(
+                                    schema = @Schema(implementation = ErrorResponseDto.class)
+                            )
+                    )
+            }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(
             @RequestParam
@@ -83,8 +176,8 @@ public class AccountController {
                     .body(new ResponseDto(STATUS_200, MESSAGE_200));
         } else {
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(STATUS_500, MESSAGE_500));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(STATUS_417, MESSAGE_417_DELETE));
         }
     }
 }
--- src/main/java/com/olvera/accounts/dto/ErrorResponseDto.java
+++ src/main/java/com/olvera/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.olvera.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,13 +11,21 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @Builder
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
 public class ErrorResponseDto {
 
+    @Schema(description = "API path invoked by client")
     private String apiPath;
 
+    @Schema(description = "Error code representing the error happened")
     private HttpStatus errorCode;
 
+    @Schema(description = "Error message representing the error happened")
     private String errorMessage;
 
+    @Schema(description = "Time representing when the error happened")
     private LocalDateTime errorTime;
 }
